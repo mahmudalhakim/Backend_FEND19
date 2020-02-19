25a2845a758f779dd7a4c02583921b0e438f252f
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2020-02-19 12:33:32 +0100

    Lektion 1

---
+++ Lektion-01/hello-world.php
@@ -0,0 +1,38 @@
+<?php
+// <?php är en PHP-tagg som startar ett skript
+# Min första PHP-sida
+
+/*
+  Datum : 2020-02-19
+  Författare: Mahmud Al Hakim
+  Copyright : MIT
+*/
+
+// Visa alla felmeddelanden och varningar
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World</title>
+</head>
+<body>
+
+<?php
+
+echo "<h1>Hello World!</h1>";
+
+print "<h2>Utskrift via funktionen print.</h2>";
+
+?>
+
+<p>Lorem ipsum dolor sit amet.</p>
+
+<?="<p>Echo - Shortcut syntax.</p>"?>
+
+</body>
+</html>
