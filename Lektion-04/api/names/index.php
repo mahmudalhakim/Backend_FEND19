934e1972d32733eaf0a77710b2040e70876d7774
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2020-02-26 19:44:20 +0100

    Lektion 4

--- Lektion-04/api/names/index.php
+++ Lektion-04/api/names/index.php
@@ -43,9 +43,17 @@ $lastNames = [
 // Ändra limit i så fall.
 // Exempel på en GET-Request
 // http://localhost/api/names/?limit=50
+
 $limit = 10;
 if(isset($_GET['limit'])){
   $limit = $_GET['limit'];
+
+  // Sätt ett felmeddelande om limit är för hög
+  // För att undvika "Fatal error:  Allowed memory size exhausted 
+  if($limit > 1000 || $limit <= 0){
+    echo '{"error":"Limit must be between 1 and 1000"}';
+    die;  // OBS! Viktigt att avsluta skriptet här
+  }
 }
 
 // 4. Skapa ett antal slumpgenererade namn 
@@ -59,7 +67,18 @@ while(true) {
     $randFnFemale = rand(0,count($firstNamesFemale)-1);
     $randLastname = rand(0,count($lastNames)-1);
 
+    // Slumpa gender, 1=male, 0=female
     $randGender = rand(0,1);
+
+    // Extra : Ange gender i URLen t.ex.
+    // http://localhost/api/names/?limit=5&gender=male
+    if(isset($_GET['gender'])){
+      if($_GET['gender'] == 'male')
+        $randGender = true;
+      elseif($_GET['gender'] == 'female')
+        $randGender = false;
+    }
+
     if($randGender){
       $firstname = $firstNamesMale[$randFnMale];
       $gender = "male";
@@ -67,17 +86,22 @@ while(true) {
       $firstname = $firstNamesFemale[$randFnMale];
       $gender = "female";
     }
+
+    // Slumpmässig ålder på varje namn mellan 1 och 100
+    $age = rand(1,100);
       
-    $name = array("firstname"=>$firstname,
-                  "lastname"=>$lastNames[$randLastname],
-                  "gender" => $gender
-                );
+    $name = array(
+      "firstname"=>$firstname,
+      "lastname"=>$lastNames[$randLastname],
+      "gender" => $gender,
+      "age" => $age
+    );
 
     $names[] = $name;
 
-    if(count($names) >= $limit)
+    if(count($names) == $limit)
       break;
-      
+
 }
 
 // Extra - Blanda alla namn i arrayen
